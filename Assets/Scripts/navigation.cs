1e55ddb2e5d9b253a8af1af7bd3396bb51015bb1
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-06-19 04:32:14 +0900

    Update navigation.cs

--- Assets/Scripts/navigation.cs
+++ Assets/Scripts/navigation.cs
@@ -20,6 +20,6 @@ public class navigation : MonoBehaviour
         float playerX = player.transform.position.x;
         float playerZ = player.transform.position.z;
 
-        CurrentPoint.transform.position = new Vector3(playerX -100, CurrentPoint.transform.position.y, playerZ - 400);
+        CurrentPoint.transform.position = new Vector3((playerX -100) * 200 / 170, CurrentPoint.transform.position.y, (playerZ - 400) * 200 / 170);
     }
 }
