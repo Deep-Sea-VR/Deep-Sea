943d7d34f8fe65c2f3e375363082b68218ef3f62
Jangmanbo <wkddpdnjs99@gmail.com>
2022-06-20 04:16:22 +0900

    add : room audio
    아이 방 BGM, 알람 소리 추가

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource childAudio;
+
+    [SerializeField]
+    private AudioSource alarmAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Data.Instance.isFindCrystal)
+            alarmAudio.Play();
+        else
+            childAudio.Play();
+    }
+
+    public void FadeOutAudio()
+    {
+        StartCoroutine("FadeOutCoroutine");
+    }
+
+    IEnumerator FadeOutCoroutine()
+    {
+        while (childAudio.volume > 0)
+        {
+            childAudio.volume -= Time.deltaTime / 10;
+            Debug.Log(childAudio.volume);
+            yield return null;
+        }
+    }
+}
